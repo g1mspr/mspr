480867835a81c283ec1e1ad39936ceea8a777758
Bahald <clement.batard.cb@proton.me>
2025-03-24 22:00:11 +0100

    Debut du projet flask pour le nester

---
+++ nesterapp/nesterapp/views.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index():
+
+    return "Hello world !"
+
+
+if __name__ == "__main__":
+
+    app.run()
+
+
+# Config options - Make sure you created a 'config.py' file.
+app.config.from_object('config')
+# To get one variable, tape app.config['MY_VARIABLE']
